91b8ad537a7124f59ee2a8569f5930e5f44ecc60
Rick <rick.zurek@gmail.com>
2016-06-30 16:20:04 -0400

    First commit

---
+++ CodeScanner/Program.cs
@@ -0,0 +1,33 @@
+﻿/* Rick Zurek
+ * 6/29/2016
+ * using USBHIDDRIVER by Florian Leitner-Fischer
+ * documentation and download avalible at: http://www.florian-leitner.de/index.php/2007/08/03/hid-usb-driver-library/
+ * 
+ * This is a simple program to automaticaly open URLs that have been scanned. 
+ * Ideally running in the background and and only activating when something is scanned.
+ * It connects to the device being used, when a code is scanned it uses a regex statment
+ * to check if the code is a url, if it is it openes that in your default browser.
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using USBHIDDRIVER;
+
+namespace CodeScanner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char input;
+
+            USBHIDDRIVER.USBInterface usb = new USBInterface("vid_C900FD", "pid_0020467086");
+
+            usb.Connect();
+
+            usb.startRead();
+        }
+    }
+}
