dea4dd6751d0f8ceab7ebc0b0d4c27d4fa232706
Rick <rick.zurek@gmail.com>
2016-07-08 16:13:25 -0400

    Fully functioning, just need to add minor touches

--- CodeScanner/Program.cs
+++ CodeScanner/Program.cs
@@ -18,7 +18,7 @@ using System.Threading;
 using System.Text.RegularExpressions;
 using System.Diagnostics;
 using System.Drawing;
-//using System.Windows.Forms;
+using System.Windows.Forms;
 using System.Runtime.InteropServices;
 
 namespace CodeScanner
@@ -26,7 +26,123 @@ namespace CodeScanner
     class Program
     {
 
+
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern IntPtr SetParent(IntPtr hWndChild, IntPtr hWndNewParent);
+
+        [DllImport("user32.dll")]
+        static extern IntPtr GetShellWindow();
+
+        [DllImport("user32.dll")]
+        static extern IntPtr GetDesktopWindow();
+
         static ManualResetEvent _quitEvent = new ManualResetEvent(false);
+
+
+        static NotifyIcon notifyIcon;
+        static IntPtr processHandle;
+        static IntPtr WinShell;
+        static IntPtr WinDesktop;
+        static MenuItem HideMenu;
+        static MenuItem RestoreMenu;
+
+        static void Main(string[] args)
+        {
+            notifyIcon = new NotifyIcon();
+            notifyIcon.Icon = new Icon("TestICON.ico");
+            notifyIcon.Text = "Monitor";
+            notifyIcon.Visible = true;
+
+            ContextMenu menu = new ContextMenu();
+            HideMenu = new MenuItem("Hide", new EventHandler(Minimize_Click));
+            RestoreMenu = new MenuItem("Restore", new EventHandler(Maximize_Click));
+
+            menu.MenuItems.Add(RestoreMenu);
+            menu.MenuItems.Add(HideMenu);
+            menu.MenuItems.Add(new MenuItem("Exit", new EventHandler(CleanExit)));
+
+            notifyIcon.ContextMenu = menu;
+
+            //You need to spin off your actual work in a different thread so that the Notify Icon works correctly
+            Task.Factory.StartNew(Run);
+
+            processHandle = Process.GetCurrentProcess().MainWindowHandle;
+
+            WinShell = GetShellWindow();
+
+            WinDesktop = GetDesktopWindow();
+
+            //Hide the Window
+            ResizeWindow(false);
+
+            ///This is required for triggering WinForms activity in Console app
+            Application.Run();
+
+
+        }
+
+        static void Run()
+        {
+            USBHIDDRIVER.USBInterface usb = new USBInterface("vid_11fa", "pid_0202");
+
+            usb.Connect();
+
+            usb.enableUsbBufferEvent(new System.EventHandler(MyEventCacher));
+
+            usb.startRead();
+
+            Console.Write("Press CTRL+C To Quit" + Environment.NewLine);
+            Console.CancelKeyPress += (sender, eArgs) =>
+            {
+                _quitEvent.Set();
+                eArgs.Cancel = true;
+            };
+
+            _quitEvent.WaitOne();
+
+            usb.stopRead();
+
+            while (true)
+            {
+                System.Threading.Thread.Sleep(1000);
+            }
+        }
+
+
+        private static void CleanExit(object sender, EventArgs e)
+        {
+            notifyIcon.Visible = false;
+            Application.Exit();
+            Environment.Exit(1);
+        }
+
+
+        static void Minimize_Click(object sender, EventArgs e)
+        {
+            ResizeWindow(false);
+        }
+
+
+        static void Maximize_Click(object sender, EventArgs e)
+        {
+            ResizeWindow();
+        }
+
+        static void ResizeWindow(bool Restore = true)
+        {
+            if (Restore)
+            {
+                RestoreMenu.Enabled = false;
+                HideMenu.Enabled = true;
+                SetParent(processHandle, WinDesktop);
+            }
+            else
+            {
+                RestoreMenu.Enabled = true;
+                HideMenu.Enabled = false;
+                SetParent(processHandle, WinShell);
+            }
+        }
 
         public static void MyEventCacher(object sender, System.EventArgs e)
         {
@@ -85,31 +201,5 @@ namespace CodeScanner
             }
 
         }
-
-        static void Main(string[] args)
-        {
-            USBHIDDRIVER.USBInterface usb = new USBInterface("vid_11fa", "pid_0202");
-
-            usb.Connect();
-
-            usb.enableUsbBufferEvent(new System.EventHandler(MyEventCacher));
-
-            usb.startRead();
-
-            Console.Write("Press CTRL+C To Quit" + Environment.NewLine);
-            Console.CancelKeyPress += (sender, eArgs) =>
-            {
-                _quitEvent.Set();
-                eArgs.Cancel = true;
-            };
-
-            _quitEvent.WaitOne();
-
-            usb.stopRead();
-
-
-        }
-
     }
 }
-
