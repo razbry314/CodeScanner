4328688b6444226de4a941f292c3aa0dd49700aa
Rick <rick.zurek@gmail.com>
2016-07-07 21:18:25 -0400

    added system tray functionality

--- CodeScanner/Program.cs
+++ CodeScanner/Program.cs
@@ -17,6 +17,9 @@ using USBHIDDRIVER;
 using System.Threading;
 using System.Text.RegularExpressions;
 using System.Diagnostics;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
 
 namespace CodeScanner
 {
@@ -83,7 +86,59 @@ namespace CodeScanner
 
         }
 
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern IntPtr SetParent(IntPtr hWndChild, IntPtr hWndNewParent);
+
+        [DllImport("user32.dll")]
+        static extern IntPtr GetShellWindow();
+
+        [DllImport("user32.dll")]
+        static extern IntPtr GetDesktopWindow();
+
+
+        static NotifyIcon notifyIcon;
+        static IntPtr processHandle;
+        static IntPtr WinShell;
+        static IntPtr WinDesktop;
+        static MenuItem HideMenu;
+        static MenuItem RestoreMenu;
+
         static void Main(string[] args)
+        {
+            notifyIcon = new NotifyIcon();
+            notifyIcon.Icon = new Icon("TestICON.ico");
+            notifyIcon.Text = "Monitor";
+            notifyIcon.Visible = true;
+
+            ContextMenu menu = new ContextMenu();
+            HideMenu = new MenuItem("Hide", new EventHandler(Minimize_Click));
+            RestoreMenu = new MenuItem("Restore", new EventHandler(Maximize_Click));
+
+            menu.MenuItems.Add(RestoreMenu);
+            menu.MenuItems.Add(HideMenu);
+            menu.MenuItems.Add(new MenuItem("Exit", new EventHandler(CleanExit)));
+
+            notifyIcon.ContextMenu = menu;
+
+            //You need to spin off your actual work in a different thread so that the Notify Icon works correctly
+            Task.Factory.StartNew(Run);
+
+            processHandle = Process.GetCurrentProcess().MainWindowHandle;
+
+            WinShell = GetShellWindow();
+
+            WinDesktop = GetDesktopWindow();
+
+            //Hide the Window
+            ResizeWindow(false);
+
+            ///This is required for triggering WinForms activity in Console app
+            Application.Run();
+
+
+        }
+
+        static void Run()
         {
             USBHIDDRIVER.USBInterface usb = new USBInterface("vid_11fa", "pid_0202");
 
@@ -104,8 +159,47 @@ namespace CodeScanner
 
             usb.stopRead();
 
+            while (true)
+            {
+                System.Threading.Thread.Sleep(1000);
+            }
         }
 
+
+        private static void CleanExit(object sender, EventArgs e)
+        {
+            notifyIcon.Visible = false;
+            Application.Exit();
+            Environment.Exit(1);
+        }
+
+
+        static void Minimize_Click(object sender, EventArgs e)
+        {
+            ResizeWindow(false);
+        }
+
+
+        static void Maximize_Click(object sender, EventArgs e)
+        {
+            ResizeWindow();
+        }
+
+        static void ResizeWindow(bool Restore = true)
+        {
+            if (Restore)
+            {
+                RestoreMenu.Enabled = false;
+                HideMenu.Enabled = true;
+                SetParent(processHandle, WinDesktop);
+            }
+            else
+            {
+                RestoreMenu.Enabled = true;
+                HideMenu.Enabled = false;
+                SetParent(processHandle, WinShell);
+            }
+        }
 
 
     }
